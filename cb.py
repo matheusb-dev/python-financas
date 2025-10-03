e8c17fb88f4d3d0289ad59f0ec1387000c90b709
matheusb-dev <leitinhogames6@gmailcom>
2025-10-03 09:58:57 -0300

    Basico de Python

---
+++ cb.py
@@ -0,0 +1,20 @@
+#Inicio
+print("Hello World")
+print(5+1)
+print(5*"oi")
+print("Oi""Te""Ba")
+#----------------------------------------- 
+
+
+#variaveis 
+#INT -> Numeros inteiros
+#FLOAT -> Numeros Quebrados
+#STRING -> Para Textos
+#----------------------------------------- 
+
+
+
+
+
+
+
