0ad87074f50018e23984afb7a624b12d7ee76a9d
matheusb-dev <leitinhogames6@gmail.com>
2025-10-05 19:39:27 -0300

    Criando tabela com Indice

---
+++ project01/ler.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+#data frame
+df = pd.read_csv('data.csv')
+
+print(df)
--- project01/main.py
+++ project01/main.py
@@ -1,26 +1,29 @@
 # NOME: MATHEUS BEZERRA DOMMINGOS
 # DATA: 04/10/2025
-# OBJETIVO: FAZER UM PROGRAMA QUE CRIE E LEIA UMA PLANILHA
+# OBJETIVO: FAZER UM PROGRAMA QUE VC CRIAR UMA PLANILHA
 
-#IMPORTANTO A LIB. PANDAS -> MANIPULLAÇÃO DE DADOS
 import pandas as pd
 
-#igual o conceito de dicionario, dados da tabela
-planilha = {
-    'NOME': ['Matheus', 'Otavio', 'Luiz', 'Gabriel'],
-    'IDADE': [19, 18, 19, 20],
-    'SALARIO': [1600, 3000, 1400, 0]
-}
+qts_de_fun = int(input("Digite a quantidade de funcionarios:... "))
+lista = []
+
+
+for x in range(qts_de_fun):
+
+    nomes = str(input("Digite o Nome:... "))
+    Idade = float(input("Digite Idade:... "))
+    Salario = float(input(f"Digite Salario:... "))
+    planilha = [nomes, Idade, Salario]
+    Folha = lista.append(planilha)
+
+
 
 # Tipo da nossa tabela
-df = pd.DataFrame(planilha)
+df = pd.DataFrame(lista)
 print("Arquivo criado com sucesso")
 
 #define nome do arquivo
-file_path = 'folha_pagamento2.xlsx'
+file_path = 'folha.csv'
 
-#escrevendo os dados na folha_pagamento
-df.to_excel(file_path, index=False) #index para não aparecer o indice no excel
-
-
-print(planilha)
+#escrevendo os dados na folha_pagamento e criando o arquivo
+df.to_csv(file_path, index=False) #index para não aparecer o indice no excel
