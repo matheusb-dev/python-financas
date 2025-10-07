dd77477abbab2ff6c4d74095552cd925179eef5a
matheusb-dev <leitinhogames6@gmailcom>
2025-10-07 08:52:07 -0300

    Dicionarios

---
+++ POO/ContaBancaria/main.py
@@ -0,0 +1,3 @@
+# NOME: MATHEUS BEZERRA DOMINGOS
+# DATA: 07/10/2025
+# OBJETIVO: Um objeto "ContaBancária" pode ter atributos como "saldo" e métodos como "depositar" e "sacar".
--- POO/main.py
+++ POO/main.py
@@ -1,1 +1,12 @@
-Class FolhaPagamento:
+class FolhaPagamento:
+    
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def exibir(self):
+        print(self.nome, self.idade)
+
+Funcionarios = FolhaPagamento("Matheus", 19)
+
+Funcionarios.exibir()
