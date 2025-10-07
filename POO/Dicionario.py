8e58fe36b45bba6f599d1aebe19bcefc9dc042d7
Matheus Bezerra Domingos <168386167+matheusb-dev@users.noreply.github.com>
2025-10-07 07:52:38 -0300

    readme

--- POO/Dicionario.py
+++ POO/Dicionario.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #Quando é apenas um
 Player = {
     'Nome': "Mat",
@@ -14,3 +15,21 @@ Relogios = [
 ]
 print(Relogios[2])
 print(Player["Money"], Player["Nome"], Player["Idade"])
+=======
+#Quando é apenas um
+Player = {
+    'Nome': "Mat",
+    'Idade': 26,
+    'Money': 10,
+}
+
+#Quando é varios
+Relogios = [
+
+    { "Marca": "Apple", "Modelo": "S9", "Preço": 2500},
+    { "Marca": "Samsung", "Modelo": "Watch 7", "Preço": 1200},
+    { "Marca": str, "Modelo": int, "Preço": int},
+]
+print(Relogios[2])
+print(Player["Money"], Player["Nome"], Player["Idade"])
+>>>>>>> 7765299e7db16bc3564a1d10e03eb2180a1e6d68
--- POO/Dicionarios.py
+++ POO/Dicionarios.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # NOME: MATHEUS BEZERRA DOMMINGOS
 # DATA: 04/10/2025
 # OBJETIVO: APRENDENDO SOBRE DICIONARIO
@@ -35,3 +36,42 @@ file_path = 'folha.csv'
 #escrevendo os dados na folha_pagamento e criando o arquivo
 df.to_csv(file_path, index=False) #index para não aparecer o indice no excel
 
+=======
+# NOME: MATHEUS BEZERRA DOMMINGOS
+# DATA: 04/10/2025
+# OBJETIVO: APRENDENDO SOBRE DICIONARIO
+
+import pandas as pd
+
+qts_de_fun = int(input("Digite a quantidade de funcionarios:... "))
+lista = []
+
+
+for x in range(qts_de_fun):
+
+    nomes = str(input("Digite o Nome:... "))
+    Idade = float(input("Digite Idade:... "))
+    Salario = float(input(f"Digite Salario:... "))
+
+    #Dicionario que defini as linhas da tabela
+    planilha = {
+        'Nome': nomes, 
+        'Idade': Idade, 
+        'Salario': Salario
+    }
+
+    lista.append(planilha)
+
+
+
+# Tipo da nossa tabela
+df = pd.DataFrame(lista)
+print("Arquivo criado com sucesso")
+
+#define nome do arquivo
+file_path = 'folha.csv'
+
+#escrevendo os dados na folha_pagamento e criando o arquivo
+df.to_csv(file_path, index=False) #index para não aparecer o indice no excel
+
+>>>>>>> 7765299e7db16bc3564a1d10e03eb2180a1e6d68
