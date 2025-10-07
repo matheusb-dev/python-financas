a23a67df604bbe01a07755d34506e77c2676ce25
matheusb-dev <leitinhogames6@gmailcom>
2025-10-07 08:20:03 -0300

    Correção

--- POO/Dicionario.py
+++ POO/Dicionario.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Quando é apenas um
 Player = {
     'Nome': "Mat",
@@ -15,21 +14,3 @@ Relogios = [
 ]
 print(Relogios[2])
 print(Player["Money"], Player["Nome"], Player["Idade"])
-=======
-#Quando é apenas um
-Player = {
-    'Nome': "Mat",
-    'Idade': 26,
-    'Money': 10,
-}
-
-#Quando é varios
-Relogios = [
-
-    { "Marca": "Apple", "Modelo": "S9", "Preço": 2500},
-    { "Marca": "Samsung", "Modelo": "Watch 7", "Preço": 1200},
-    { "Marca": str, "Modelo": int, "Preço": int},
-]
-print(Relogios[2])
-print(Player["Money"], Player["Nome"], Player["Idade"])
->>>>>>> 7765299e7db16bc3564a1d10e03eb2180a1e6d68
--- POO/main.py
+++ POO/main.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 Class FolhaPagamento:
-=======
-Class FolhaPagamento:
->>>>>>> 7765299e7db16bc3564a1d10e03eb2180a1e6d68
