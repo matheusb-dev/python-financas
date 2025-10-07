4672d874b57ca2c1840dbeadbb08b4f3f8aca1c5
matheusb-dev <leitinhogames6@gmailcom>
2025-10-07 09:03:10 -0300

    1° Object

--- POO/ContaBancaria/main.py
+++ POO/ContaBancaria/main.py
@@ -1,3 +1,24 @@
 # NOME: MATHEUS BEZERRA DOMINGOS
 # DATA: 07/10/2025
 # OBJETIVO: Um objeto "ContaBancária" pode ter atributos como "saldo" e métodos como "depositar" e "sacar".
+
+class ContaBancaria:
+
+    def __init__(self, saldo):
+        self.saldo = saldo
+
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def exibir(self):
+        print(self.saldo)
+    
+Matheus = ContaBancaria(50)
+
+
+Matheus.depositar(80)
+
+Matheus.exibir()
+
+        
